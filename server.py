5de210b359e0e9e1536edb6e4685a5bb9e83c896
alix <alice.bystrih@yandex.ru>
2020-06-05 23:40:12 +0300

    Update server with python news

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+
+from news_python import get_python_news
 from weather import weather_by_city
 
 app = Flask(__name__)
@@ -8,7 +10,8 @@ app = Flask(__name__)
 def index():
     page_title = 'News Python'
     weather = weather_by_city('Moscow', 'Russia')
-    return render_template('index.html', page_title=page_title, weather=weather)
+    news = get_python_news()
+    return render_template('index.html', page_title=page_title, weather=weather, news_list=news)
 
 
 if __name__ == "__main__":
