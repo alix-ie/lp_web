26c30b8fb14ad2f5f57f2ab8be9d4da9cf4245f6
alix <alice.bystrih@yandex.ru>
2020-07-20 22:14:31 +0300

    SQLite usage

--- web/__init__.py
+++ web/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 from web.news_python import get_python_news
 from web.weather import weather_by_city
+from web.model import db
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    db.init_app(app)
 
     @app.route("/")
     def index():
