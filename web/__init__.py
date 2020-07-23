650facc8fef4c70019bb8df872630ad539ab87bd
alix <alice.bystrih@yandex.ru>
2020-07-23 23:32:54 +0300

    Add flask-login support to the app

--- web/__init__.py
+++ web/__init__.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user, current_user
 
 from web.forms import LoginForm
 from web.weather import weather_by_city
-from web.model import db, News
+from web.model import db, News, User
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
 
     @app.route("/")
     def index():
@@ -19,8 +28,30 @@ def create_app():
 
     @app.route('/login')
     def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+
         title = 'Authorization'
         login_form = LoginForm()
         return render_template('login.html', page_title=title, form=login_form)
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Logged in.')
+                return redirect(url_for('index'))
+
+        flash('The username or password that you have entered is invalid.')
+        return redirect(url_for('login'))
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Logged out.')
+        return redirect(url_for('index'))
 
     return app
