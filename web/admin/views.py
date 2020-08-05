ebbed2b9afc4798804eef4bef7352eff9980afdc
alix <alice.bystrih@yandex.ru>
2020-08-05 14:08:10 +0300

    Add admin_required decorator

--- web/admin/views.py
+++ web/admin/views.py
@@ -1,13 +1,11 @@
-from flask import Blueprint
-from flask_login import current_user, login_required
+from flask import Blueprint, render_template
+from web.user.decorators import admin_required
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
 
 @blueprint.route('/')
-@login_required
+@admin_required
 def admin_index():
-    if current_user.is_admin:
-        return 'Hey, admin!'
-    else:
-        return "I don't like you."
+    title = 'Control Panel'
+    return render_template('admin/index.html', page_title=title)
