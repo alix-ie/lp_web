d976291698176bf4e8171c95057825adc7ddd96b
alix <alice.bystrih@yandex.ru>
2023-05-04 17:52:12 +0400

    Add single news view

--- web/news/views.py
+++ web/news/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, current_app, render_template
+from flask import abort, Blueprint, current_app, render_template
 
 from web.weather import weather_by_city
 from web.news.models import News
@@ -10,5 +10,15 @@ blueprint = Blueprint('news', __name__)
 def index():
     title = 'News Python'
     weather_report = weather_by_city(current_app.config['WEATHER_DEFAULT_CITY'])
-    news = News.query.order_by(News.published.desc()).all()
+    news = News.query.filter(News.text.isnot(None)).order_by(News.published.desc()).all()
     return render_template('news/index.html', page_title=title, weather=weather_report, news_list=news)
+
+
+@blueprint.route('/news/<int:news_id>')
+def single_news(news_id):
+    news = News.query.filter(News.id == news_id).first()
+
+    if not news:
+        abort(404)
+
+    return render_template('news/single_news.html', page_title=news.title, news=news)
